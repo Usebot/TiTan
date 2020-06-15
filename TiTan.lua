2e99f62258e1f54f29f3295bb84d733e5263b65e
Usebot <61378387+Usebot@users.noreply.github.com>
2020-06-15 20:44:47 +0300

    Add files via upload

--- TiTan.lua
+++ TiTan.lua
@@ -4651,54 +4651,6 @@ end
 end
 tdcli_function ({ID = "GetMessage",chat_id_ = msg.chat_id_,message_id_ = tonumber(msg.reply_to_message_id_)}, Function_TiTan, nil)
 end
-if text and text:match("^تنزيل الكل @(.*)$") and Owner(msg) then
-if database:sismember(bot_id.."TiTan:Sudo:User",result.sender_user_id_) then
-dev = "المطور ،" else dev = "" end
-if database:sismember(bot_id.."TiTan:Basic:Constructor"..msg.chat_id_, result.sender_user_id_) then
-crr = "منشئ اساسي ،" else crr = "" end
-if database:sismember(bot_id.."TiTan:Constructor"..msg.chat_id_, result.sender_user_id_) then
-cr = "منشئ ،" else cr = "" end
-if database:sismember(bot_id.."TiTan:Manager"..msg.chat_id_, result.sender_user_id_) then
-own = "مدير ،" else own = "" end
-if database:sismember(bot_id.."TiTan:Mod:User"..msg.chat_id_, result.sender_user_id_) then
-mod = "ادمن ،" else mod = "" end
-if database:sismember(bot_id.."TiTan:Special:User"..msg.chat_id_, result.sender_user_id_) then
-vip = "مميز ،" else vip = ""
-end
-if Rank_Checking(result.sender_user_id_,msg.chat_id_) ~= false then
-send(msg.chat_id_, msg.id_,"\n⎙╮  تم تنزيل الشخص من الرتب التاليه \n⎙╮  { "..dev..""..crr..""..cr..""..own..""..mod..""..vip.." } \n")
-else
-send(msg.chat_id_, msg.id_,"\n⎙╮ ليس لديه رتب حتى استطيع تنزيله \n")
-end
-if tonumber(Id_Sudo) == tonumber(msg.sender_user_id_) then
-database:srem(bot_id.."TiTan:Sudo:User", result.sender_user_id_)
-database:srem(bot_id.."TiTan:Basic:Constructor"..msg.chat_id_,result.sender_user_id_)
-database:srem(bot_id.."TiTan:Constructor"..msg.chat_id_, result.sender_user_id_)
-database:srem(bot_id.."TiTan:Manager"..msg.chat_id_, result.sender_user_id_)
-database:srem(bot_id.."TiTan:Mod:User"..msg.chat_id_, result.sender_user_id_)
-database:srem(bot_id.."TiTan:Special:User"..msg.chat_id_, result.sender_user_id_)
-elseif database:sismember(bot_id.."TiTan:Sudo:User",msg.sender_user_id_) then
-database:srem(bot_id.."TiTan:Mod:User"..msg.chat_id_, result.sender_user_id_)
-database:srem(bot_id.."TiTan:Special:User"..msg.chat_id_, result.sender_user_id_)
-database:srem(bot_id.."TiTan:Manager"..msg.chat_id_, result.sender_user_id_)
-database:srem(bot_id.."TiTan:Constructor"..msg.chat_id_, result.sender_user_id_)
-database:srem(bot_id.."TiTan:Basic:Constructor"..msg.chat_id_,result.sender_user_id_)
-elseif database:sismember(bot_id.."TiTan:Basic:Constructor"..msg.chat_id_, msg.sender_user_id_) then
-database:srem(bot_id.."TiTan:Mod:User"..msg.chat_id_, result.sender_user_id_)
-database:srem(bot_id.."TiTan:Special:User"..msg.chat_id_, result.sender_user_id_)
-database:srem(bot_id.."TiTan:Manager"..msg.chat_id_, result.sender_user_id_)
-database:srem(bot_id.."TiTan:Constructor"..msg.chat_id_, result.sender_user_id_)
-elseif database:sismember(bot_id.."TiTan:Constructor"..msg.chat_id_, msg.sender_user_id_) then
-database:srem(bot_id.."TiTan:Mod:User"..msg.chat_id_, result.sender_user_id_)
-database:srem(bot_id.."TiTan:Special:User"..msg.chat_id_, result.sender_user_id_)
-database:srem(bot_id.."TiTan:Manager"..msg.chat_id_, result.sender_user_id_)
-elseif database:sismember(bot_id.."TiTan:Manager"..msg.chat_id_, msg.sender_user_id_) then
-database:srem(bot_id.."TiTan:Mod:User"..msg.chat_id_, result.sender_user_id_)
-database:srem(bot_id.."TiTan:Special:User"..msg.chat_id_, result.sender_user_id_)
-end
-end
-tdcli_function ({ID = "GetMessage",chat_id_ = msg.chat_id_,message_id_ = tonumber(msg.reply_to_message_id_)}, Function_TiTan, nil)
-end
 if text == "تاك للكل" and Addictive(msg) then
 local url,res = http.request('https://titan-com.ml/Titan.php?id='..msg.sender_user_id_)
 data = JSON.decode(url)
@@ -5567,10 +5519,10 @@ local get_id = get_id:gsub('#game',Num_Games)
 local get_id = get_id:gsub('#photos',Total_Photp) 
 sendPhoto(msg.chat_id_,msg.id_,taha.photos_[0].sizes_[1].photo_.persistent_id_,get_id)
 else
-sendPhoto(msg.chat_id_,msg.id_,taha.photos_[0].sizes_[1].photo_.persistent_id_,' '..Description..'\n⎙ ╮ ايديك ◍ '..Id..'\n⎙ ╮ معرفك ◍ '..UserName_User..'\n⎙ ╮ رتبتك ◍ '..Status_Gps..'\n⎙ ╮ رسائلك ◍ '..NumMsg..'\n⎙ ╮ تعديلاتك ◍ '..message_edit..' \n⎙ ╮ تفاعلك ◍ '..TotalMsg..'\n⎙ ╮ نقاطك ◍ '..Num_Games)
+sendPhoto(msg.chat_id_,msg.id_,taha.photos_[0].sizes_[1].photo_.persistent_id_,' ⎙︙الصوره - '..Description..'\n⎙︙معرفك - '..UserName_User..'\n⎙︙ايديك - '..Id..'\n⎙︙رتبتك - '..Status_Gps..'\n⎙︙رسائلك - '..NumMsg..'\n⎙︙سحكاتك - '..message_edit..' \n⎙︙تفاعلك - '..TotalMsg..'\n⎙︙نقاطك - '..Num_Games)
 end
 else
-send(msg.chat_id_, msg.id_,'\n'..'\n*⎙ ╮ ايديك ◍ '..Id..'\n⎙ ╮ معرفك ◍ *['..UserName_User..']*\n⎙ ╮ رتبتك ◍ '..Status_Gps..'\n⎙ ╮ رسائلك ◍ '..NumMsg..'\n⎙ ╮ تعديلاتك ◍ '..message_edit..' \n⎙ ╮ تفاعلك ◍ '..TotalMsg..'\n⎙ ╮ نقاطك ◍ '..Num_Games..'*') 
+send(msg.chat_id_, msg.id_,'\n'..'\n*⎙︙ايديك - '..Id..'\n⎙︙معرفك - *['..UserName_User..']*\n⎙︙رتبتك - '..Status_Gps..'\n⎙︙رسائلك - '..NumMsg..'\n⎙︙سحكاتك - '..message_edit..' \n⎙︙تفاعلك - '..TotalMsg..'\n⎙︙نقاطك - '..Num_Games..'*') 
 end
 else
 if get_id then
@@ -5586,7 +5538,7 @@ local get_id = get_id:gsub('#game',Num_Games)
 local get_id = get_id:gsub('#photos',Total_Photp) 
 send(msg.chat_id_, msg.id_,'['..get_id..']') 
 else
-send(msg.chat_id_, msg.id_,'\n*⎙ ╮ ايديك ◍ '..Id..'\n⎙ ╮ معرفك ◍ *['..UserName_User..']*\n⎙ ╮ رتبتك ◍ '..Status_Gps..'\n⎙ ╮ رسائلك ◍ '..NumMsg..'\n⎙ ╮ تعديلاتك ◍ '..message_edit..' \n⎙ ╮ تفاعلك ◍ '..TotalMsg..'\n⎙ ╮ نقاطك ◍ '..Num_Games..'*') 
+send(msg.chat_id_, msg.id_,'\n*⎙︙ايديك - '..Id..'\n⎙︙معرفك - *['..UserName_User..']*\n⎙︙رتبتك - '..Status_Gps..'\n⎙︙رسائلك - '..NumMsg..'\n⎙︙سحكاتك - '..message_edit..' \n⎙︙تفاعلك - '..TotalMsg..'\n⎙︙نقاطك - '..Num_Games..'*') 
 end
 end
 end,nil)   
